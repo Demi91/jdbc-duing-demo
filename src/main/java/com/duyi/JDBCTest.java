dc223003604efdf40a382f831b7b43c5b3084d5b
Demi91 <lanluo_bingzi@126.com>
2020-03-28 14:53:14 +0800

    init

---
+++ src/main/java/com/duyi/JDBCTest.java
@@ -0,0 +1,40 @@
+package com.duyi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBCTest {
+
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    public static final String URL = "jdbc:mysql://localhost:3306/test";
+
+    public static final String USER = "root";
+
+    public static final String PASS = "123456";
+
+
+    public static void main(String[] args) throws Exception {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet result = null;
+
+        Class.forName(DRIVER);
+        conn = DriverManager.getConnection(URL,USER,PASS);
+        stmt = conn.createStatement();
+        result = stmt.executeQuery("show variables like 'basedir'");
+
+        while (result.next()){
+            String name = result.getString("variable_name");
+            String value = result.getString("value");
+            System.out.println(name + " " + value);
+        }
+
+        stmt.close();
+        result.close();
+        conn.close();
+
+    }
+}
